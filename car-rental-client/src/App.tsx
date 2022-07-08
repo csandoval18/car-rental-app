11206dd3657fa81b9327061fa60876efb0167680
csandoval18 <christian_alvites@outlook.com>
2022-07-08 00:10:03 -0500

    first commit

---
+++ car-rental-client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+	return (
+		<div className='App'>
+			<div className='text-3xl font-bold underline'>Hello World!</div>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat commodi
+				laboriosam similique, accusantium vitae in architecto, sequi ratione,
+				animi nisi ipsa beatae non et sunt ut esse nemo velit aut.
+			</div>
+		</div>
+	)
+}
+
+export default App
